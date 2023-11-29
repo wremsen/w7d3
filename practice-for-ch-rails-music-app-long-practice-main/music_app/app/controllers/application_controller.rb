19f25a9fea3da62db1a5e45d120d51bae120f2fb
wremsen <wcremsen@gmail.com>
2023-11-29 14:07:56 -0500

    set up new page

--- practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/application_controller.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/application_controller.rb
@@ -1,2 +1,36 @@
+# CR(R)LLL - ApplicationController
+    # current_user
+    # require_logged_in
+    # require_logged_out
+    # login!(user)
+    # logged_in?
+    # logout
+
+
+
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in
+        !!current_user
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def require_logged_in
+        redirect_to new_session_url unless logged_in?
+    end
+
+    def require_logged_out
+        redirect_to user_url if logged_in?
+    end
+
+
+
 end
--- practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/sessions_controller.rb
@@ -1,3 +1,18 @@
 class SessionsController < ApplicationController
+
+
+    def create
+        email = params[:user][:email]
+        password = params[:user][:password]
+
+        @user = User.find_by_credentials(email, password)
+
+        if @user.save
+            redirect_to user_url
+        else
+            render json: errors.full_messages, status: 422
+        end
+    end
+    
     
 end
