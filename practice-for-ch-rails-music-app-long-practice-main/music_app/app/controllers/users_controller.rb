c341e0fb4ad8dc59bde7e557f4a4708abd13baac
Inkorrect-Code <ntongprofessional@gmail.com>
2023-11-29 14:17:14 -0500

    finished user.rb and user controller

---
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user= User.new(user_params)
+         if @user.save 
+             redirect_to user_url # redirect to page
+         else
+             render json: errors.full_messages, status: 422
+         end
+     end
+ 
+     private
+ 
+     def user_params
+         params.require(:user).permit(:username, :password)
+     end
+end
--- practice-for-ch-rails-music-app-long-practice-main/music_app/app/models/user.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/models/user.rb
@@ -1,2 +1,38 @@
 class User < ApplicationRecord
+    before_validation :ensure_session_token
+
+    validates :email, uniqueness: true, presence: true
+    validates :session_token, presence: true 
+    validates :password, length: {minimum: 6}, allow_nil: true
+
+    attr_reader :password
+
+    def find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        pass_obj = BCrypt::Password.new(self.password_digest)
+        pass_obj.is_password?(password)
+    end
+    
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafebase64
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
 end
